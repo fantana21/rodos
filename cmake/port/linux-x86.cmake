2138c3c1053c715e4e67e6655f8ea63fb743d098
Patrick Kappl <patrick.kappl@fantana.at>
2026-01-22 10:55:06 +0100

    Set default toolchain file in Linux-x86 and VA41620 ports

--- cmake/port/linux-x86.cmake
+++ cmake/port/linux-x86.cmake
@@ -1,2 +1,9 @@
 set(port_dir "bare-metal/linux-x86")
 set(is_port_baremetal TRUE)
+
+# Set a default toolchain file
+set(CMAKE_TOOLCHAIN_FILE
+    "${CMAKE_CURRENT_LIST_DIR}/../toolchains/linux-x86.cmake"
+    CACHE FILEPATH
+    "Toolchain file for cross-compiling"
+)
