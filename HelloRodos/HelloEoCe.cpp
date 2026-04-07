8736dd4ac6d145193e7180e7d4bc2db62ab30f9a
Patrick Kappl <patrick.kappl@fantana.at>
2026-04-07 14:15:16 +0200

    Go back to counting interrupts on RX pin of debug UART

--- HelloRodos/HelloEoCe.cpp
+++ HelloRodos/HelloEoCe.cpp
@@ -46,45 +46,48 @@ class InterruptTest : public RODOS::StaticThread<> {
     auto run() -> void override {
         static constexpr auto interruptCountingInterval = 200 * RODOS::MILLISECONDS;
 
-        // debugUartRxPin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
-        pf4Pin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
-        pf5Pin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
-        // debugUartRxPin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
-        pf4Pin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
-        pf5Pin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
+        debugUartRxPin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
+        // pf4Pin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
+        // pf5Pin.init(/*isOutput=*/false, /*numOfPins=*/1, /*initVal=*/0);
+        debugUartRxPin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
+        // pf4Pin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
+        // pf5Pin.config(RODOS::GPIO_CFG_TYPE::GPIO_CFG_IRQ_SENSITIVITY, RODOS::GPIO_IRQ_SENS_BOTH);
 
         RODOS::PRINTF("\n");
         RODOS::PRINTF("Waiting for both edge interrupts on debug UART RX pin\n");
-        auto interruptCounterPf4 = InterruptCounter{};
-        auto interruptCounterPf5 = InterruptCounter{};
+        auto interruptCounter = InterruptCounter{};
+        // auto interruptCounterPf4 = InterruptCounter{};
+        // auto interruptCounterPf5 = InterruptCounter{};
         while(true) {
-            interruptCounterPf4.nInterrupts = 0;
-            interruptCounterPf5.nInterrupts = 0;
-            // debugUartRxPin.resetInterruptEventStatus();
-            pf4Pin.resetInterruptEventStatus();
-            pf5Pin.resetInterruptEventStatus();
+            interruptCounter.nInterrupts = 0;
+            // interruptCounterPf4.nInterrupts = 0;
+            // interruptCounterPf5.nInterrupts = 0;
+            debugUartRxPin.resetInterruptEventStatus();
+            // pf4Pin.resetInterruptEventStatus();
+            // pf5Pin.resetInterruptEventStatus();
 
-            // debugUartRxPin.setIoEventReceiver(&interruptCounter);
-            pf4Pin.setIoEventReceiver(&interruptCounterPf4);
-            pf5Pin.setIoEventReceiver(&interruptCounterPf5);
+            debugUartRxPin.setIoEventReceiver(&interruptCounter);
+            // pf4Pin.setIoEventReceiver(&interruptCounterPf4);
+            // pf5Pin.setIoEventReceiver(&interruptCounterPf5);
 
-            // debugUartRxPin.interruptEnable(true);
-            pf4Pin.interruptEnable(true);
-            pf5Pin.interruptEnable(true);
+            debugUartRxPin.interruptEnable(true);
+            // pf4Pin.interruptEnable(true);
+            // pf5Pin.interruptEnable(true);
 
-            // debugUartRxPin.suspendUntilDataReady(RODOS::END_OF_TIME);
-            pf4Pin.suspendUntilDataReady(RODOS::END_OF_TIME);
+            debugUartRxPin.suspendUntilDataReady(RODOS::END_OF_TIME);
+            // pf4Pin.suspendUntilDataReady(RODOS::END_OF_TIME);
             RODOS::AT(RODOS::NOW() + interruptCountingInterval);
 
-            // debugUartRxPin.interruptEnable(false);
-            pf4Pin.interruptEnable(false);
-            pf5Pin.interruptEnable(false);
+            debugUartRxPin.interruptEnable(false);
+            // pf4Pin.interruptEnable(false);
+            // pf5Pin.interruptEnable(false);
 
-            // debugUartRxPin.setIoEventReceiver(nullptr);
-            pf4Pin.setIoEventReceiver(nullptr);
-            pf5Pin.setIoEventReceiver(nullptr);
-            RODOS::PRINTF("%3i interrupt(s) occured on PF4\n", interruptCounterPf4.nInterrupts);
-            RODOS::PRINTF("%3i interrupt(s) occured on PF5\n", interruptCounterPf5.nInterrupts);
+            debugUartRxPin.setIoEventReceiver(nullptr);
+            // pf4Pin.setIoEventReceiver(nullptr);
+            // pf5Pin.setIoEventReceiver(nullptr);
+            RODOS::PRINTF("%3i interrupt(s) occured\n", interruptCounter.nInterrupts);
+            // RODOS::PRINTF("%3i interrupt(s) occured on PF4\n", interruptCounterPf4.nInterrupts);
+            // RODOS::PRINTF("%3i interrupt(s) occured on PF5\n", interruptCounterPf5.nInterrupts);
         }
     }
 } interruptTest;
