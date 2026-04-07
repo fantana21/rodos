4bc9f61a8851d34f6a2f5b3812bd5b919e5c1abf
Patrick Kappl <patrick.kappl@fantana.at>
2026-04-07 11:21:06 +0200

    Route putchar() through newlib's _write() in VA41620 port
    Unfortunately, `_write()` is not standard, but glibc, musl, newlib (and
    thereby also newlib-nano, and picolibc) all use `_write()` so this
    should be fine.

--- src/bare-metal/va41620/hw/syscalls.cpp
+++ src/bare-metal/va41620/hw/syscalls.cpp
@@ -1,28 +1,18 @@
-#include "hal/hal_uart.h"
-#include "platform-parameter.h"
-#include "scheduler.h"
 #include "rodos-debug.h"
-#include "hw_uart_dbg.h"
 #include "hw_specific.h"
 #include "yprintf.h"
-#include "cortex_m/peripheral_defs.h"
+
+
+extern "C" int _write(int file, char * ptr, int len);
+
 
 namespace RODOS {
-
-bool isInInterrupt();
-
 int putchar(int ic) {
-    const char c = static_cast<char>(ic);
+    char c = static_cast<char>(ic);
     if (c == '\n') {
         putchar('\r');
     }
-    if(isInInterrupt()){
-        HW_HAL_UART::putcharFromInterrupt(static_cast<VA41620_UART_IDX>(UART_DEBUG_IDX), c);
-    }else{
-        while(uartStdout.putcharNoWait(c) == -1){
-            FFLUSH();
-        }
-    }
+    _write(1, &c, 1);
     return c;
 }
 
@@ -37,24 +27,5 @@ void abort(void) { while(1); }
 
 void sp_partition_yield() {}
 
-static Semaphore fflushSemaphore{};
-
-void FFLUSH() {
-    if(isSchedulerRunning()&&!isInInterrupt()){
-        ScopeProtector fflushProtector{&fflushSemaphore};
-        uartStdout.suspendUntilWriteFinished();
-    }else{
-        while (!uartStdout.isWriteFinished());
-    }
-}
-
-int getCurrentIRQ(){
-    using namespace SCB_ICSR;
-    return static_cast<int>(scb.ICSR.read<VECTACTIVE>())-16;
-}
-
-bool isInInterrupt(){
-    return getCurrentIRQ()!=-16;
-}
-
+void FFLUSH() {}
 }
